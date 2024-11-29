054e0617c70be241f8d29cb5e4eab6042f1e30e3
Nazar <je3nsoff@icloud.com>
2024-11-29 19:33:14 +0200

    Update settings.py

--- store/settings.py
+++ store/settings.py
@@ -137,7 +137,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'miraclemc.team@gmail.com'
-EMAIL_HOST_PASSWORD = 'wgbp zzym sgwn fali'
+EMAIL_HOST_PASSWORD = 'APP PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
